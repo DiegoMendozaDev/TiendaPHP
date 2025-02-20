b3854ae253ce196ae411164ac1af25878e23bf66
Carlos <carlos.carra7@gmail.com>
2025-02-20 17:27:24 +0100

    detalle crud terminado 3

--- src/Entity/DetallePedido.php
+++ src/Entity/DetallePedido.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Repository\DetallePedidoRepository;
+
 use Doctrine\ORM\Mapping as ORM;
 use Doctrine\DBAL\Types\Types;
 use App\Repository\DetallePedidoRepository;
