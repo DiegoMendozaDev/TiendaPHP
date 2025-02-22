c2449a950f835ff2d7af6ccd7f045c05dfb08c78
DiegoMendozaDev <you@example.com>
2025-02-22 14:33:40 +0100

    algunos productos

--- client/app/views/productos/index.php
+++ client/app/views/productos/index.php
@@ -246,20 +246,24 @@
       </div>
       <div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
-          <h2 class="display-5">Another headline</h2>
-          <p class="lead">And an even wittier subheading.</p>
+          <h2 class="display-5"><?= $productos[2]->nombre ?></h2>
+          <p class="lead"><?= $productos[2]->descripcion ?></p>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-dark shadow-sm mx-auto" style="border-radius: 21px 21px 0 0;">
+          <img src=<?= $productos[2]->foto ?> class="img-fluid">  
+        </div>
       </div>
     </div>
 
     <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
       <div class="bg-body-tertiary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 p-3">
-          <h2 class="display-5">Another headline</h2>
-          <p class="lead">And an even wittier subheading.</p>
+          <h2 class="display-5"><?= $productos[1]->nombre ?></h2>
+          <p class="lead"><?= $productos[1]->descripcion ?></p>
         </div>
-        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        <div class="bg-dark shadow-sm mx-auto" style="border-radius: 21px 21px 0 0;">
+          <img src=<?= $productos[1]->foto ?> class="img-fluid">  
+        </div>
       </div>
       <div class="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
         <div class="my-3 py-3">
